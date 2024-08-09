a78aac087ab32d7655463276bb3db8eb27ba8cdc
xiewen <1123654054@qq.com>
2024-08-09 21:11:58 +0800

    定义别名修改配置文件

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import { setupRouter } from "./router";
+import "@/style.css";
+import App from "@/App.vue";
+import { setupRouter } from "@/router";
 
 function bootstrap() {
   const app = createApp(App);
